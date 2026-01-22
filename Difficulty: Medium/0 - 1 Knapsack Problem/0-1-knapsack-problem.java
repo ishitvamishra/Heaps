c36174f20f8d9d6b7784088fde91cde1a91f2764
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-01-22 10:47:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -2,16 +2,17 @@ class Solution {
     public int knapsack(int W, int val[], int wt[]) {
         // code here
         int n = wt.length;
-        int m = val.length;
         int dp[][] = new int[n+1][W+1];
     
         
         for(int i = 1; i<=n; i++){
             for(int j = 1; j<=W;j++){
+                //balls are less than what player has to play
                 if(j<wt[i-1]){
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = dp[i-1][j];   //previous runs that was scored without current player
                 }
                 else{
+                    //if he plays
                     dp[i][j] = Math.max(dp[i-1][j], val[i-1]+dp[i-1][j-wt[i-1]]);
                 }
             }
